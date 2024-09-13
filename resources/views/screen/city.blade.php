a134e388705fb79fc9e2ef0cf0bc4d7d95ef4cfd
Reham Shaban <79982223+reham-shaban@users.noreply.github.com>
2024-09-13 23:17:39 +0300

    null on website

--- resources/views/screen/city.blade.php
+++ resources/views/screen/city.blade.php
@@ -72,12 +72,18 @@
                     container.innerHTML = ''; // Clear the container before adding new data
 
                     data.forEach(item => {
+                        if(item.description){
+                            description = item.description
+                        }
+                        else{
+                            description = ""
+                        }
                         container.innerHTML += `
                             <a class="category-card" href='/courses/${item.slug}'>
                             <img src="${item.media_url}" alt="${item.image_alt}">
                             <div class="card-overlay">
                                 <h3>${item.title}</h3>
-                                <p>${item.description}</p>
+                                <p>${description}</p>
                                 <span class="line-card"></span>
                                 <span class="category-card-arrow"><img src="/assets/icons/arrow.svg" alt=""></span>
                             </div>
